78c611296879fefca4c3f8642455cd8739225cd2
luannabarrete <71093861+luannabarrete@users.noreply.github.com>
2020-12-05 17:46:18 -0300

    Update deploy-amdocs.sh

--- deploy-amdocs.sh
+++ deploy-amdocs.sh
@@ -50,7 +50,7 @@ if ls /root/project/nginx.conf/nginx.conf
         then mv /root/project/nginx.conf/nginx.conf /etc/nginx/nginx.conf
         
         else
-                git clone https://github.com/luannabarrete/nginx.conf.git /root/project/nginx.conf
+                git clone https://github.com/luannabarrete/nginx.conf.git /root/project/nginx.conf/nginx.conf
                 mv /root/project/nginx.conf/nginx.conf /etc/nginx/nginx.conf
 fi
 
