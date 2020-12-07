5eb0ccc3bb7f9cdef4ec3e69a82ecfb07827fd42
luannabarrete <71093861+luannabarrete@users.noreply.github.com>
2020-12-07 08:50:18 -0300

    Update deploy-amdocs.sh

--- deploy-amdocs.sh
+++ deploy-amdocs.sh
@@ -4,13 +4,12 @@
 
 if ls /etc/nginx > /dev/null 2>&1
 
-        then
-                echo "_________________________________________________________\n\nNGINX is already installed."
-                
-        else
-        
-                apt install nginx -y &&
-                echo "_________________________________________________________\n\nNGINX installed successfully.\n_________________________________________________________\n"
+    then
+        echo "_________________________________________________________\n\nNGINX is already installed."
+            
+    else
+        apt install nginx -y &&
+        echo "_________________________________________________________\n\nNGINX installed successfully.\n_________________________________________________________\n"
                 
 fi                
 
@@ -19,10 +18,12 @@ fi
 
 if ls /usr/share/nginx/dox > /dev/null 2>&1
 
-        then
-                echo "_________________________________________________________\n\nThe repository 'dox' already exists in /usr/share/nginx.\n_________________________________________________________\n"
-        else
-                git clone https://github.com/uknbr/dox.git /usr/share/nginx/dox && echo "_________________________________________________________\nThe repository 'dox' has been successfully cloned to /usr/share/nginx!\n______________________________________________________\n"
+    then
+        echo "_________________________________________________________\n\nThe repository 'dox' already exists in /usr/share/nginx.\n_________________________________________________________\n"
+    
+    else
+        git clone https://github.com/uknbr/dox.git /usr/share/nginx/dox && echo "_________________________________________________________\nThe repository 'dox' has been successfully cloned to /usr/share/nginx!\n______________________________________________________\n"
+
 fi
 
 # !!! DEPLOY WITH NGINX !!!
@@ -30,50 +31,49 @@ fi
 # If the configuration file is found...
 if ls /etc/nginx/sites-available/infratest.com > /dev/null 2>&1
 
-# ...so display that it is already in the directory...
+    # ...so display that it is already in the directory...
+    then
+        echo "\nThe configuration file 'infratest.com' is already in /etc/nginx/sites-available/ and /etc/nginx/sites-enabled/.\n______________________________________________________\n"
+
+    # ...otherwise check for the existence of the "infratest" repository. If it exists, just move it to the correct directory, otherwise make a git clone of it and move the file to the correct directory. After that create an identical file in /etc/nginx/sites-enabled/ and establish a link between the two.
+    elif ls /root/project/infratest > /dev/null
+
         then
-                echo "\nThe configuration file 'infratest.com' is already in /etc/nginx/sites-available/ and /etc/nginx/sites-enabled/.\n______________________________________________________\n"
-
-# ...otherwise check for the existence of the "infratest" repository. If it exists, just move it to the correct directory, otherwise make a git clone of it and move the file to the correct directory. After that create an identical file in /etc/nginx/sites-enabled/ and establish a link between the two.
-
-        elif ls /root/project/infratest > /dev/null
-
-        then
-                mv /root/project/infratest/infratest.com /etc/nginx/sites-available/ &&
-                ln -s /etc/nginx/sites-available/infratest.com /etc/nginx/sites-enabled/infratest.com &&
-                echo "Configuration file moved successfully."
+            mv /root/project/infratest/infratest.com /etc/nginx/sites-available/ &&
+            ln -s /etc/nginx/sites-available/infratest.com /etc/nginx/sites-enabled/infratest.com &&
+            echo "Configuration file moved successfully."
 
         else
 
-                git clone https://github.com/luannabarrete/infratest.git /root/project/infratest &&
-                mv /root/project/infratest/infratest.com /etc/nginx/sites-available/ &&
-                ln -s /etc/nginx/sites-available/infratest.com /etc/nginx/sites-enabled/infratest.com &&
-                echo "Configuration file cloned and moved successfully."
+            git clone https://github.com/luannabarrete/infratest.git /root/project/infratest &&
+            mv /root/project/infratest/infratest.com /etc/nginx/sites-available/ &&
+            ln -s /etc/nginx/sites-available/infratest.com /etc/nginx/sites-enabled/infratest.com &&
+            echo "Configuration file cloned and moved successfully."
 fi
 
 # Delete default files:
 if ls /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default > /dev/null 2>&1
-
-        then rm /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+    
+    then rm /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
         
-        else echo ""
-        
+    else echo ""
+    
 fi
 
 #Validate the existence of nginx.conf file and moving it to /etc/nginx/:
 if ls /root/project/config-file-nginx/nginx.conf > /dev/null 2>&1
 
-        then 
-                echo "The repository 'config-file-nginx' already exists in /root/project/.\n"
+    then 
+        echo "The repository 'config-file-nginx' already exists in /root/project/.\n"
         
-        else
-                rm -fr /root/project/config-file-nginx/
-                git clone https://github.com/luannabarrete/config-file-nginx.git /root/project/config-file-nginx/ > /dev/null 2>&1 &&
-                echo "The configuration file has been cloned successfully!\n"
-                mv -f /root/project/config-file-nginx/nginx.conf /etc/nginx/nginx.conf
-                echo "\nConfiguration file 'nginx.conf' moved to /etc/nginx/ successfully!\n"
-                rm -fr /root/project/config-file-nginx/
-                git clone https://github.com/luannabarrete/config-file-nginx.git /root/project/config-file-nginx/ > /dev/null 2>&1
+    else
+        rm -fr /root/project/config-file-nginx/
+        git clone https://github.com/luannabarrete/config-file-nginx.git /root/project/config-file-nginx/ > /dev/null 2>&1 &&
+        echo "The configuration file has been cloned successfully!\n"
+        mv -f /root/project/config-file-nginx/nginx.conf /etc/nginx/nginx.conf
+        echo "\nConfiguration file 'nginx.conf' moved to /etc/nginx/ successfully!\n"
+        rm -fr /root/project/config-file-nginx/
+        git clone https://github.com/luannabarrete/config-file-nginx.git /root/project/config-file-nginx/ > /dev/null 2>&1
 fi
 
 echo "________________________________________________________________________________________________________________\n"
@@ -84,9 +84,9 @@ echo "__________________________________________________________________________
 
 if curl localhost:8080 > /dev/null 2>&1
 
-        then    
-                systemctl status nginx
-                echo "\n
+    then    
+        systemctl status nginx
+        echo "\n
 
  █████╗ ███╗   ███╗██████╗  ██████╗  ██████╗███████╗
 ██╔══██╗████╗ ████║██╔══██╗██╔═══██╗██╔════╝██╔════╝
@@ -94,9 +94,10 @@ if curl localhost:8080 > /dev/null 2>&1
 ██╔══██║██║╚██╔╝██║██║  ██║██║   ██║██║     ╚════██║
 ██║  ██║██║ ╚═╝ ██║██████╔╝╚██████╔╝╚██████╗███████║
 ╚═╝  ╚═╝╚═╝     ╚═╝╚═════╝  ╚═════╝  ╚═════╝╚══════╝\n_________________________\n\nTHE SITE IS UP!!! :D\n_________________________\n"
-        else    
-                echo "\n_________________________\n\nTHE SITE IS DOWN!!! D:\n\nRestarting site...\n_________________________\n"
-                systemctl restart nginx
-                systemctl status nginx
+    else    
+        echo "\n_________________________\n\nTHE SITE IS DOWN!!! D:\n\nRestarting site...\n_________________________\n"
+        systemctl restart nginx
+        systemctl status nginx
+        
 fi
         
